9bb6cefce8ba32ba8f1fae322ce30bb23dfde93f
Your Name <you@example.com>
2018-05-10 23:44:42 +0900

    first commit

---
+++ python/simple-cipher/simple_cipher.py
@@ -0,0 +1,14 @@
+class Cipher(object):
+    def __init__(self, key=None):
+        pass
+
+    def encode(self, text):
+        pass
+
+    def decode(self, text):
+        pass
+
+
+class Caesar(object):
+    def __init__(self):
+        pass
