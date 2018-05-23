b67db99c5640e9ae439ab3641373261162b081ea
cr4ftsm4n <mou.hangun@gmail.com>
2018-05-23 12:14:38 +0900

    update word_count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 
 
+def add_word(d, w):
+    word = w.strip("'")
+    if word:
+        d[word] += 1
+
+
 def word_count(phrase):
     word_count = defaultdict(int)
     new_word = ""
-    for c in phrase:
-        c = c.lower()
+    for c in phrase.lower():
         if c == "," or c == "_" or c.isspace():
-            word_count[new_word.strip("'")] += 1
+            add_word(word_count, new_word)
             new_word = ""
         elif (c >= 'a' and c <= 'z') or (c >= '1' and c <= '9') or c == "'":
             new_word += c
-    word_count[new_word.strip("'")] += 1
-    word_count.pop("", None)
+    # 最後の単語忘れないよに
+    add_word(word_count, new_word)
     return word_count
