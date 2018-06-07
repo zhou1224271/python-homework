96cf77aa44e5e90639547dce74f7bda950a9bb2c
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-06-07 11:33:55 +0900

    Update anagram.py

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,20 @@
+from collections import defaultdict
+
+
+def to_dict(word):
+    d = defaultdict(int)
+    for c in word:
+        d[c] += 1
+    return d
+
+
 def detect_anagrams(word, candidates):
-    pass
+    targets = []
+    word = word.lower()
+    for target in candidates:
+        lower_target = target.lower()
+        if lower_target == word:
+            pass
+        elif to_dict(word) == to_dict(lower_target):
+            targets.append(target)
+    return targets
