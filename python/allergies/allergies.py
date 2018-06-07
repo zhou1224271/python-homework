484aeab599478e54762182630e90fa6d20fd6f07
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-06-07 11:37:10 +0900

    Update allergies.py

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,22 @@
 class Allergies(object):
-
     def __init__(self, score):
-        pass
+        self.allergies = ["eggs", "peanuts", "shellfish",
+                          "strawberries", "tomatoes", "chocolate", "pollen", "cats"]
+        self.score = score
 
     def is_allergic_to(self, item):
-        pass
+        if item in self.allergies:
+            n = self.allergies.index(item)
+            n = 2 ** n
+            return n & self.score != 0
+        else:
+            return False
 
     @property
     def lst(self):
-        pass
+        allergies = []
+        for item in self.allergies:
+            if self.is_allergic_to(item):
+                allergies.append(item)
+
+        return allergies
