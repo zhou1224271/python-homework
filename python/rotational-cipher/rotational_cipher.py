64df0a23319ab077bc76924cbf00efe06cb94f4b
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-06-07 11:23:34 +0900

    Update rotational_cipher.py

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,22 @@
+from string import ascii_lowercase, ascii_uppercase
+
+
+def shift_char(c, l, k):
+    new_ord = ord(c) + k
+    if new_ord > ord(l[-1]):
+        new_ord = new_ord - len(l)
+    return chr(new_ord)
+
+
 def rotate(text, key):
-    pass
+    new_str = ""
+    for c in text:
+        if c in ascii_lowercase:
+            new_c = shift_char(c, ascii_lowercase, key)
+            new_str += new_c
+        elif c in ascii_uppercase:
+            new_c = shift_char(c, ascii_uppercase, key)
+            new_str += new_c
+        else:
+            new_str += c
+    return new_str
