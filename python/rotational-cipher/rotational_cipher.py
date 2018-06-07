e250108e496358080dc8a2afde61c6bdc3277ea9
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-06-07 22:54:52 +0900

    Update rotational_cipher.py

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,20 +1,31 @@
 from string import ascii_lowercase, ascii_uppercase
 
 
-def shift_char(c, l, k):
-    new_ord = ord(c) + k
-    if new_ord > ord(l[-1]):
-        new_ord = new_ord - len(l)
-    return chr(new_ord)
+# def shift_char(c, l, k):
+#     new_ord = ord(c) + k
+#     if new_ord > ord(l[-1]):
+#         new_ord = new_ord - len(l)
+#     return chr(new_ord)
 
+
+# def rotate(text, key):
+#     new_str = ""
+#     for c in text:
+#         if c in ascii_lowercase:
+#             new_str += shift_char(c, ascii_lowercase, key)
+#         elif c in ascii_uppercase:
+#             new_str += shift_char(c, ascii_uppercase, key)
+#         else:
+#             new_str += c
+#     return new_str
 
 def rotate(text, key):
-    new_str = ""
+    l = ascii_lowercase * 2 + ascii_uppercase * 2
+    ret = ""
     for c in text:
-        if c in ascii_lowercase:
-            new_str += shift_char(c, ascii_lowercase, key)
-        elif c in ascii_uppercase:
-            new_str += shift_char(c, ascii_uppercase, key)
+        i = l.find(c)
+        if i >= 0:
+            ret += l[i+key]
         else:
-            new_str += c
-    return new_str
+            ret += c
+    return ret
