75758d65dcb8ad79385e91654b1165a1fbe13bda
cr4ftsm4n <mou.hangun@gmail.com>
2018-06-14 17:16:49 +0900

    add new solution

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,3 +1,24 @@
+# from string import ascii_lowercase
+
+
+# def base_trans(text):
+#     dest = ''
+#     for c in text:
+#         if c.isalnum():
+#             dest += c
+#     return dest.lower().translate(str.maketrans(ascii_lowercase, ascii_lowercase[::-1]))
+
+
+# def encode(plain):
+#     LENGHT = 5
+#     cipher = base_trans(plain)
+#     l = [(cipher[i:i + LENGHT]) for i in range(0, len(cipher), LENGHT)]
+#     return " ".join(l)
+
+
+# def decode(ciphered):
+#     return base_trans(ciphered)
+
 from string import ascii_lowercase, digits
 
 
