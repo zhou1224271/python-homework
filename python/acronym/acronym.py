0ddf1744aecc211914ac84b6032d63a0f62396ed
Your Name <you@example.com>
2018-06-21 20:36:07 +0900

    update acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,2 @@
 def abbreviate(words):
-    return "".join(map(lambda x: x[0].upper(), words.replace(",", " ").replace("-", " ").split()))
+    return "".join(map(lambda x: x[0:1].upper(), words.replace("-", " ").split()))
