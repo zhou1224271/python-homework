f374f7c0bcebfb6691b557b765fb2523433c916d
cr4ftsm4n <mou.hangun@gmail.com>
2018-06-28 15:25:12 +0900

    update roman_numerals

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,3 +1,14 @@
+# def numeral(number):
+#     num_list=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+#     str_list=["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+#     res=''
+#     for i in range(len(num_list)):
+#         while number>=num_list[i]:
+#             number-=num_list[i]
+#             res+=str_list[i]
+#     return res
+
+
 def get_display(h, one, five, ten):
     return ["", one, one * 2, one * 3, one + five, five, five + one, five + one * 2, five + one * 3, one + ten][h]
 
